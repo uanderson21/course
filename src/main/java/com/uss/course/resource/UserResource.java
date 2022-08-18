33955e071b176c2720b0922c412441c3c0c34add
Uanderson Santana <udsantos@indracompany.com>
2022-08-18 07:38:59 -0300

    User entity and resource

---
+++ src/main/java/com/uss/course/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.uss.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uss.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L,"Uanderson", "uanderson21@yahoo.com.br", "981057444", "brutal");
+		return ResponseEntity.ok(u);
+	}
+	
+}
