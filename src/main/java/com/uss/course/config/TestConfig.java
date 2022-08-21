e146cab9c4ebc9707b8dc409bed57a57dd855e47
Uanderson Santana <udsantos@indracompany.com>
2022-08-21 19:59:01 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/uss/course/config/TestConfig.java
+++ src/main/java/com/uss/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.uss.course.entities.Category;
 import com.uss.course.entities.Order;
+import com.uss.course.entities.Product;
 import com.uss.course.entities.User;
 import com.uss.course.entities.enums.OrderStatus;
 import com.uss.course.repositories.CategoryRepository;
 import com.uss.course.repositories.OrderRepository;
+import com.uss.course.repositories.ProductRepository;
 import com.uss.course.repositories.UserRepository;
 
 @Configuration
@@ -26,6 +28,8 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;
 	@Autowired
 	private CategoryRepository categoryRepository;	
+	@Autowired
+	private ProductRepository productRepository;		
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,9 +37,24 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));		
 		
-		// TODO Auto-generated method stub
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));			
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));	
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);		
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Uanderson Santana", "uanderson21@yahoo.com.br", "981057444", "brutal1");
 		User u2 = new User(null, "Renata Castro Santana", "rezinhacbu21@yahoo.com.br", "981057445", "brutal2");
 		User u3 = new User(null, "Lizzie Castro Santana", "lizzie21@yahoo.com.br", "981057446", "brutal3");		
