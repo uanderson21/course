61b905b125b082d3d2d89ff9750247530428fe35
Uanderson Santana <udsantos@indracompany.com>
2022-08-23 07:46:19 -0300

    User update

--- src/main/java/com/uss/course/services/UserService.java
+++ src/main/java/com/uss/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.uss.course.entities.User;
@@ -31,4 +32,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}	
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+	  entity.setName(obj.getName());
+	  entity.setEmail(obj.getEmail());
+	  entity.setPhone(obj.getPhone());
+	}
 }
