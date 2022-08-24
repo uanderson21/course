64f3ef5433112f95efa77e90cff355a36fd9e282
Uanderson Santana <udsantos@indracompany.com>
2022-08-24 07:26:08 -0300

    Exception handling - findById

---
+++ src/main/java/com/uss/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.uss.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+}
